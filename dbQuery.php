2abb224e3fbea3c700ab498626209ebf29a5aef9
Shakeel Khan <khanshak9@hotmail.com>
2018-05-27 00:03:15 +1200

    proper db query

--- dbQuery.php
+++ dbQuery.php
@@ -1,69 +1,56 @@
 <?php
-    function processMessage($update) { 
+    function processMessage($request) {         
             $dbString = "pgsql:"
-            . "ec2-54-235-66-24.compute-1.amazonaws.com;"
-            . "dbname=d5tfc77ecvjh96;"
-            . "user=ytchabybvuaeog;"
-            . "port=5432;"
-            . "sslmode=require;"
-            . "password=f457e5a75151626f6a23493485673b3ea75595a7dcbcfd3b7994fc9d7f1e53d5";
-        
+                        . "host=ec2-54-235-66-24.compute-1.amazonaws.com;"
+                        . "dbname=d5tfc77ecvjh96;"
+                        . "user=ytchabybvuaeog;"
+                        . "port=5432;"
+                        . "sslmode=require;"
+                        . "password=f457e5a75151626f6a23493485673b3ea75595a7dcbcfd3b7994fc9d7f1e53d5";
                 $db = new PDO($dbString); 
-                if($update["result"]["action"] == "DBtest" ){
-                $paper = $update["result"]["parameters"]["paper1"];
-                if(empty($paper)){
-                    $paper = $update["result"]["parameters"]["paper2"];
-                    if(empty($paper)){
-                        $paper = $update["result"]["parameters"]["paper3"];
-                    }
-                }
                 
-                $query = "SELECT * FROM papers WHERE prereq LIKE '%$paper%'";
-                $result = $db->query($query);
-                /*if($row["prereq"] == "NONE" || $row["prereq"] == " NONE"){
-                    sendMessage(array(
-                        "source" => $update["result"]["source"],
-                        "speech" => $paper . " is not a requirement for any papers.",
-                        "displayText" => $paper . " is not a requirement for any papers.",
-                        "contextOut" => array()
-                    ));
-                }*/
-                else{
-                    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                if ($request["queryResult"]["action"] == "DBLink" ) {
+                    $papercode = $request["queryResult"]["parameters"]["paper1"];
+                    
+                    $query = "SELECT * FROM papers WHERE papercode LIKE '%$papercode'";
+                    $dbres = $db->query($query);
+
+                    $pLevel = $pCode = $pName = $pPoints = $pCos = $pPres = "default";
+                    while ($row = $dbres->fetch(PDO::FETCH_ASSOC)) {
                         $pLevel = $row["paperlevel"];
                         $pCode = $row["papercode"];
                         $pName = $row["papername"];
                         $pPoints = $row["paperpoints"];
-                        $pPre = $row["paperpre"];
-                        $pCor = $row["papercos"];                        
+                        $pCos = $row["papercos"];
+                        $pPres = $row["paperpros"];
                     }
-                        $result->closeCursor();
-                        
-                        sendMessage(array(
-                            "source" => $update["result"]["source"],
-                            "speech" => $paper. ". Name: " .$pName. ". Points: ". $pPoints,
-                            "displayText" => $paper. ". Name: " .$pName. ". Points: ". $pPoints,
-                            "contextOut" => array()
-                        ));
-                    
-                        sendMessage(array(
-                            "source" => $update["result"]["source"],
-                            "speech" => $paper . " is a Requirement for: " . $pName,
-                            "displayText" => $paper . " is a Requirement for: " . $pName,
-                            "contextOut" => array()
-                        ));
-                    
-                }  
+
+                    $dbres->closeCursor();
+
+                    $speech = $pName . ", worth " . $pPoints . " points";
+                    $text = $pName . ", worth " . $pPoints . " points";
+
+                    $response = new \stdClass();
+                    $response->fulfillmentText = $text;
+                    $response->source = $update["queryResult"]["source"];
+                    sendMessage($response);
+                    // sendMessage( array (
+                    //     "source" => $update["queryResult"]["source"],
+                    //     "speech" => $pName . ", worth " . $pPoints . " points",
+                    //     "displayText" => $pName . ", worth " . $pPoints . " points",
+                    //     "contextOut" => array()
+                    // ) );
+                }
             }
-        }
         
         function sendMessage($parameters) {
             echo json_encode($parameters);
         }
         
-        $update_response = file_get_contents("php://input");
-        $update = json_decode($update_response, true);
-        if (isset($update["result"]["action"])) {
-            processMessage($update);
+        $request_response = file_get_contents("php://input");
+        $request = json_decode($request_response, true);
+        if (isset($request["queryResult"]["action"])) {
+            processMessage($request);
         }
+    
 ?>
